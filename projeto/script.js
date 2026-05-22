4f839859c6fb0e06bfd2bc39e6a342967013479a
Ruana Vieira <ruana-rossi@hotmail.com>
2026-05-21 21:09:45 -0300

    Atualizações

--- projeto/script.js
+++ projeto/script.js
@@ -1,9 +1,27 @@
-function abrirImagem(imagem) {
-  document.getElementById("modal").style.display = "block";
-
-  document.getElementById("imagemModal").src = imagem;
+function openModal(id) {
+  document.getElementById(id).style.display = "flex";
 }
 
-function fecharImagem() {
-  document.getElementById("modal").style.display = "none";
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
 }
+
+function openImage(src) {
+  document.getElementById("viewerImage").src = src;
+
+  document.getElementById("imageViewer").style.display = "flex";
+}
+
+function closeImage() {
+  document.getElementById("imageViewer").style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modals = document.querySelectorAll(".modal");
+
+  modals.forEach((modal) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+};
